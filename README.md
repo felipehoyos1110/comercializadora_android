d3fe1eaaa75fc0f5af668fea545d9c37e8e96dc3
luisfelipehoyos <luis.hoyos@mercadolibre.com.co>
2023-02-27 16:03:41 -0500

    initial project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Análisis de plataformas para la implementación de una solución de software.
+Se desarrolla una aplicación móvil para clientes en Android (java) y SQLite.
+
+![technology java](https://img.shields.io/badge/technology-java-blue.svg)
+
+![technology java](https://img.shields.io/badge/technology-anndroid-blue.svg)
+
+## Responsables
+
+- Luis Felipe Hoyos Londoño
+- Jhon Fredy Duarte Vargas
+
+### Plataformas de Desarrollo de Software
+
+Semana 7
+
+PROFESOR: FRANK JAIRO CASTILLO PADILLA
+
+Febrero-2023
---
+++ app/src/main/java/com/cdp/comercializadora/VerActivity.java
@@ -0,0 +1,100 @@
+package com.cdp.comercializadora;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.cdp.comercializadora.db.DbClientes;
+import com.cdp.comercializadora.entidades.Clientes;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class VerActivity extends AppCompatActivity {
+
+    EditText txtNombre, txtTelefono, txtCorreo;
+    Button btnGuarda;
+    FloatingActionButton fabEditar, fabEliminar;
+
+    Clientes cliente;
+    int id = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver);
+
+        txtNombre = findViewById(R.id.txtNombre);
+        txtTelefono = findViewById(R.id.txtTelefono);
+        txtCorreo = findViewById(R.id.txtCorreoElectronico);
+        fabEditar = findViewById(R.id.fabEditar);
+        fabEliminar = findViewById(R.id.fabEliminar);
+        btnGuarda = findViewById(R.id.btnGuarda);
+        btnGuarda.setVisibility(View.INVISIBLE);
+
+        if(savedInstanceState == null){
+            Bundle extras = getIntent().getExtras();
+            if(extras == null){
+                id = Integer.parseInt(null);
+            } else {
+                id = extras.getInt("ID");
+            }
+        } else {
+            id = (int) savedInstanceState.getSerializable("ID");
+        }
+
+        final DbClientes dbClientes = new DbClientes(VerActivity.this);
+        cliente = dbClientes.verCliente(id);
+
+        if(cliente != null){
+            txtNombre.setText(cliente.getNombre());
+            txtTelefono.setText(cliente.getTelefono());
+            txtCorreo.setText(cliente.getCorreo_electornico());
+            txtNombre.setInputType(InputType.TYPE_NULL);
+            txtTelefono.setInputType(InputType.TYPE_NULL);
+            txtCorreo.setInputType(InputType.TYPE_NULL);
+        }
+
+        fabEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(VerActivity.this, EditarActivity.class);
+                intent.putExtra("ID", id);
+                startActivity(intent);
+            }
+        });
+
+        fabEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(VerActivity.this);
+                builder.setMessage("¿Desea eliminar este cliente?")
+                        .setPositiveButton("SI", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                                if(dbClientes.eliminarCliente(id)){
+                                    lista();
+                                }
+                            }
+                        })
+                        .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        }).show();
+            }
+        });
+    }
+
+    private void lista(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
